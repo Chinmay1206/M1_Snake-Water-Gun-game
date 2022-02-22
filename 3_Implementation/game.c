8d467fc36b86861a7937de0ae0b4de1548375809
Chinmay1206 <chinmaybonde09@gmail.com>
2022-02-22 23:43:16 +0530

    init

--- 3_Implementation/game.c
+++ 3_Implementation/game.c
@@ -1,50 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include "game.h"
 
-int snakeWaterGun(char you, char comp){
-    // returns 1 if you win, -1 if you lose and 0 if draw
-    // Condition for draw
-    // Cases covered:
-    // ss
-    // gg
-    // ww
-    if(you == comp){
-        return 0;
-    }
-
-    // Non-draw conditions
-    // Cases covered:
-    // sg
-    // gs
-    // sw 
-    // ws
-    // gw
-    // wg
-    
-
-    if(you=='s' && comp=='g'){
-        return -1;
-    }
-    else if(you=='g' && comp=='s'){
-        return 1;
-    }
-
-    if(you=='s' && comp=='w'){
-        return 1;
-    }
-    else if(you=='w' && comp=='s'){
-        return -1;
-    }
-
-    if(you=='g' && comp=='w'){
-        return -1;
-    }
-    else if(you=='w' && comp=='g'){
-        return 1;
-    }
-
-}
+extern int snakeWaterGun(char you, char comp);
 int main(){
     char you, comp;
     srand(time(0));
