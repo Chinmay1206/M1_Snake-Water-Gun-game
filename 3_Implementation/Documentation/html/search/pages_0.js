30d4ec551cab8c3acb667e9fcd880dc8db555419
Chinmay1206 <chinmaybonde09@gmail.com>
2022-03-03 14:41:04 +0530

    init

---
+++ 3_Implementation/Documentation/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['readme_0',['Readme',['../md__c___users__lenovo__one_drive__desktop__project_1__m1__snake__water__gun_game_3__implementation_src__readme.html',1,'']]]
+];
--- 3_Implementation/inc/game.h
+++ 3_Implementation/inc/game.h
@@ -1,5 +1,22 @@
+/**
+ * @file Snake_Water_gun
+ * @author Chinmay Bonde (chinmaybonde09@gmail.com)
+ * @brief Snake Water Gun is one of the famous two-player game played by many people. It is a hand game in which the player randomly chooses any of the three forms i.e. snake, water, and gun
+ * @version 0.1
+ * @date 2022-03-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #ifndef GAME_H
 #define GAME_H
+/**
+ * @brief Function of the game Snake_Water_Gun
+ * 
+ * @param you 
+ * @param comp 
+ * @return int 
+ */
 int snakeWaterGun(char you, char comp);
 
 #endif
